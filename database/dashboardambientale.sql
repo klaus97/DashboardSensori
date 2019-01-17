7bd4d46da4ac7c5bbb8b854cb706a051f85a89f9
klaus97 <raffaele830@gmail.com>
2019-01-17 14:25:21 +0100

    update db

--- database/dashboardambientale.sql
+++ database/dashboardambientale.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Gen 10, 2019 alle 13:00
+-- Creato il: Gen 17, 2019 alle 14:24
 -- Versione del server: 10.1.32-MariaDB
 -- Versione PHP: 7.2.5
 
@@ -61,21 +61,25 @@ INSERT INTO `dato` (`ID`, `valore`, `tipo`) VALUES
 (1, 12, 'temperatura'),
 (2, 45, 'umidità'),
 (6, 10, 'temperatura'),
-(2009, -17, 'temperatura'),
-(2010, -11, 'temperatura'),
-(2011, 46, 'umidità'),
-(2012, 29, 'temperatura'),
-(2013, -9, 'temperatura'),
-(2014, 47, 'umidità'),
-(2015, -1, 'temperatura'),
-(2016, 28, 'temperatura'),
-(2017, 21, 'umidità'),
-(2018, -18, 'temperatura'),
-(2019, -11, 'temperatura'),
-(2020, 58, 'umidità'),
-(2021, -14, 'temperatura'),
-(2022, 2, 'temperatura'),
-(2023, 73, 'umidità');
+(2036, 12, 'temperatura'),
+(2037, 31, 'temperatura'),
+(2038, -7, 'temperatura'),
+(2039, 37, 'temperatura'),
+(2040, -1, 'temperatura'),
+(2041, -6, 'temperatura'),
+(2042, 5, 'temperatura'),
+(2043, 90, 'umidità'),
+(2044, -1, 'temperatura'),
+(2045, 25, 'temperatura'),
+(2046, 15, 'temperatura'),
+(2047, 19, 'temperatura'),
+(2048, 5, 'temperatura'),
+(2049, 19, 'temperatura'),
+(2050, 39, 'temperatura'),
+(2051, 29, 'umidità'),
+(2052, 4, 'temperatura'),
+(2053, -7, 'temperatura'),
+(2054, 61, 'umidità');
 
 -- --------------------------------------------------------
 
@@ -95,7 +99,8 @@ CREATE TABLE `edificio` (
 --
 
 INSERT INTO `edificio` (`ID`, `nome`, `indirizzo`, `ID_zona`) VALUES
-(1, 'Univeristà Coppito', 'Via vetoio,48', 1);
+(1, 'Univeristà Coppito', 'Via vetoio,48', 1),
+(2, 'Università Coppito 0', 'Via vetoio ', 1);
 
 -- --------------------------------------------------------
 
@@ -107,6 +112,13 @@ CREATE TABLE `gestisce` (
   `ID_gestore` int(10) UNSIGNED NOT NULL,
   `ID_zona` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `gestisce`
+--
+
+INSERT INTO `gestisce` (`ID_gestore`, `ID_zona`) VALUES
+(2, 1);
 
 -- --------------------------------------------------------
 
@@ -129,7 +141,9 @@ CREATE TABLE `gestore` (
 INSERT INTO `gestore` (`ID`, `codiceacc`, `nome`, `cognome`, `ID_area`) VALUES
 (1, 'AQ12', 'carlo', 'conti', 1),
 (2, 'AQZ19', 'Paul', 'prova', 1),
-(3, 'AQEL20', 'test', 'pass', 1);
+(3, 'AQEL20', 'test', 'pass', 1),
+(4, 'AQEL21', 'pippo', 'peppo', 1),
+(5, 'AQZ20', 'prova', 'bebe', 1);
 
 -- --------------------------------------------------------
 
@@ -149,31 +163,35 @@ CREATE TABLE `invia` (
 
 INSERT INTO `invia` (`ID_sensore`, `ID_dato`, `datainvio`) VALUES
 (1, 1, '2018-12-22'),
-(1, 2009, '2019-01-09'),
-(1, 2012, '2019-01-09'),
-(1, 2015, '2019-01-09'),
-(1, 2018, '2019-01-09'),
-(1, 2021, '2019-01-09'),
+(1, 2042, '2019-01-16'),
 (2, 2, '2018-12-27'),
-(2, 2011, '2019-01-09'),
-(2, 2014, '2019-01-09'),
-(2, 2017, '2019-01-09'),
-(2, 2020, '2019-01-09'),
-(2, 2023, '2019-01-09'),
+(2, 2043, '2019-01-16'),
+(2, 2051, '2019-01-16'),
+(2, 2054, '2019-01-16'),
 (3, 6, '2018-12-25'),
-(3, 2010, '2019-01-09'),
-(3, 2013, '2019-01-09'),
-(3, 2016, '2019-01-09'),
-(3, 2019, '2019-01-09'),
-(3, 2022, '2019-01-09');
+(3, 2037, '2019-01-16'),
+(3, 2038, '2019-01-16'),
+(3, 2040, '2019-01-16'),
+(3, 2041, '2019-01-16'),
+(3, 2050, '2019-01-16'),
+(3, 2053, '2019-01-16'),
+(4, 2036, '2019-01-16'),
+(4, 2039, '2019-01-16'),
+(4, 2044, '2019-01-16'),
+(4, 2045, '2019-01-16'),
+(4, 2046, '2019-01-16'),
+(4, 2047, '2019-01-16'),
+(4, 2048, '2019-01-16'),
+(4, 2049, '2019-01-16'),
+(4, 2052, '2019-01-16');
 
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `luogo aperto`
+-- Struttura della tabella `luogoaperto`
 --
 
-CREATE TABLE `luogo aperto` (
+CREATE TABLE `luogoaperto` (
   `ID` int(11) UNSIGNED NOT NULL,
   `nome` varchar(35) NOT NULL,
   `indirizzo` varchar(50) NOT NULL,
@@ -181,23 +199,47 @@ CREATE TABLE `luogo aperto` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dump dei dati per la tabella `luogo aperto`
+-- Dump dei dati per la tabella `luogoaperto`
 --
 
-INSERT INTO `luogo aperto` (`ID`, `nome`, `indirizzo`, `ID_zona`) VALUES
+INSERT INTO `luogoaperto` (`ID`, `nome`, `indirizzo`, `ID_zona`) VALUES
 (1, 'Lago Vetoio', 'Str. Lago di Vetoio', 2);
 
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `monitora`
+-- Struttura della tabella `monitoraed`
 --
 
-CREATE TABLE `monitora` (
-  `ID_luogoaperto` int(10) UNSIGNED NOT NULL,
+CREATE TABLE `monitoraed` (
   `ID_gestore` int(10) UNSIGNED NOT NULL,
   `ID_edificio` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `monitoraed`
+--
+
+INSERT INTO `monitoraed` (`ID_gestore`, `ID_edificio`) VALUES
+(4, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `monitoraluogo`
+--
+
+CREATE TABLE `monitoraluogo` (
+  `ID_gestore` int(10) UNSIGNED NOT NULL,
+  `ID_luogoaperto` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `monitoraluogo`
+--
+
+INSERT INTO `monitoraluogo` (`ID_gestore`, `ID_luogoaperto`) VALUES
+(3, 1);
 
 -- --------------------------------------------------------
 
@@ -218,7 +260,9 @@ CREATE TABLE `ruolo` (
 INSERT INTO `ruolo` (`ID`, `tipo`, `ID_gestore`) VALUES
 (1, 'area', 1),
 (2, 'zona', 2),
-(3, 'luogo', 3);
+(3, 'luogo', 3),
+(4, 'luogo', 4),
+(5, 'zona', 5);
 
 -- --------------------------------------------------------
 
@@ -233,17 +277,18 @@ CREATE TABLE `sensore` (
   `massimale` int(11) NOT NULL,
   `freq` int(11) NOT NULL,
   `ID_luogoaperto` int(10) UNSIGNED DEFAULT NULL,
-  `ID_edificio` int(10) UNSIGNED DEFAULT NULL
+  `ID_stanza` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `sensore`
 --
 
-INSERT INTO `sensore` (`ID`, `cod`, `stato`, `massimale`, `freq`, `ID_luogoaperto`, `ID_edificio`) VALUES
-(1, 'TEMP12', 0, 22, 0, NULL, NULL),
-(2, 'UM56', 0, 40, 2, NULL, NULL),
-(3, 'TEMP13', 0, 18, 0, NULL, NULL);
+INSERT INTO `sensore` (`ID`, `cod`, `stato`, `massimale`, `freq`, `ID_luogoaperto`, `ID_stanza`) VALUES
+(1, 'TEMP12', 0, 22, 0, 1, NULL),
+(2, 'UM56', 1, 33, 1, NULL, 1),
+(3, 'TEMP13', 0, 22, 0, NULL, 1),
+(4, 'TEMP14', 1, 20, 0, NULL, 2);
 
 -- --------------------------------------------------------
 
@@ -257,6 +302,14 @@ CREATE TABLE `stanza` (
   `piano` smallint(6) DEFAULT NULL,
   `ID_edificio` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `stanza`
+--
+
+INSERT INTO `stanza` (`ID`, `nome`, `piano`, `ID_edificio`) VALUES
+(1, 'C1.10', 1, 1),
+(2, 'A1.6', 2, 2);
 
 -- --------------------------------------------------------
 
@@ -324,19 +377,25 @@ ALTER TABLE `invia`
   ADD KEY `dato_invia` (`ID_dato`);
 
 --
--- Indici per le tabelle `luogo aperto`
+-- Indici per le tabelle `luogoaperto`
 --
-ALTER TABLE `luogo aperto`
+ALTER TABLE `luogoaperto`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `luogoaperto_zona` (`ID_zona`);
 
 --
--- Indici per le tabelle `monitora`
+-- Indici per le tabelle `monitoraed`
 --
-ALTER TABLE `monitora`
-  ADD PRIMARY KEY (`ID_luogoaperto`,`ID_gestore`),
-  ADD KEY `monitora_edificio` (`ID_edificio`),
-  ADD KEY `monitora_gestore` (`ID_gestore`);
+ALTER TABLE `monitoraed`
+  ADD PRIMARY KEY (`ID_gestore`,`ID_edificio`),
+  ADD KEY `monitoraed_edificio` (`ID_edificio`);
+
+--
+-- Indici per le tabelle `monitoraluogo`
+--
+ALTER TABLE `monitoraluogo`
+  ADD PRIMARY KEY (`ID_gestore`,`ID_luogoaperto`),
+  ADD KEY `monitoraluogo_luogoaperto` (`ID_luogoaperto`);
 
 --
 -- Indici per le tabelle `ruolo`
@@ -350,7 +409,9 @@ ALTER TABLE `ruolo`
 --
 ALTER TABLE `sensore`
   ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `cod` (`cod`);
+  ADD UNIQUE KEY `cod` (`cod`),
+  ADD KEY `sensore_luogoaperto` (`ID_luogoaperto`),
+  ADD KEY `sensore_stanza` (`ID_stanza`);
 
 --
 -- Indici per le tabelle `stanza`
@@ -380,43 +441,43 @@ ALTER TABLE `area`
 -- AUTO_INCREMENT per la tabella `dato`
 --
 ALTER TABLE `dato`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2024;
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2055;
 
 --
 -- AUTO_INCREMENT per la tabella `edificio`
 --
 ALTER TABLE `edificio`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT per la tabella `gestore`
 --
 ALTER TABLE `gestore`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT per la tabella `luogo aperto`
+-- AUTO_INCREMENT per la tabella `luogoaperto`
 --
-ALTER TABLE `luogo aperto`
+ALTER TABLE `luogoaperto`
   MODIFY `ID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT per la tabella `ruolo`
 --
 ALTER TABLE `ruolo`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT per la tabella `sensore`
 --
 ALTER TABLE `sensore`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT per la tabella `stanza`
 --
 ALTER TABLE `stanza`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT per la tabella `zona`
@@ -455,24 +516,37 @@ ALTER TABLE `invia`
   ADD CONSTRAINT `sensore_invia` FOREIGN KEY (`ID_sensore`) REFERENCES `sensore` (`ID`);
 
 --
--- Limiti per la tabella `luogo aperto`
+-- Limiti per la tabella `luogoaperto`
 --
-ALTER TABLE `luogo aperto`
+ALTER TABLE `luogoaperto`
   ADD CONSTRAINT `luogoaperto_zona` FOREIGN KEY (`ID_zona`) REFERENCES `zona` (`ID`);
 
 --
--- Limiti per la tabella `monitora`
+-- Limiti per la tabella `monitoraed`
 --
-ALTER TABLE `monitora`
-  ADD CONSTRAINT `monitora_edificio` FOREIGN KEY (`ID_edificio`) REFERENCES `edificio` (`ID`),
-  ADD CONSTRAINT `monitora_gestore` FOREIGN KEY (`ID_gestore`) REFERENCES `gestore` (`ID`),
-  ADD CONSTRAINT `monitora_luogoaperto` FOREIGN KEY (`ID_luogoaperto`) REFERENCES `luogo aperto` (`ID`);
+ALTER TABLE `monitoraed`
+  ADD CONSTRAINT `monitoraed_edificio` FOREIGN KEY (`ID_edificio`) REFERENCES `edificio` (`ID`),
+  ADD CONSTRAINT `monitoraed_gestore` FOREIGN KEY (`ID_gestore`) REFERENCES `gestore` (`ID`);
+
+--
+-- Limiti per la tabella `monitoraluogo`
+--
+ALTER TABLE `monitoraluogo`
+  ADD CONSTRAINT `monitoraluogo_gestore` FOREIGN KEY (`ID_gestore`) REFERENCES `gestore` (`ID`),
+  ADD CONSTRAINT `monitoraluogo_luogoaperto` FOREIGN KEY (`ID_luogoaperto`) REFERENCES `luogoaperto` (`ID`);
 
 --
 -- Limiti per la tabella `ruolo`
 --
 ALTER TABLE `ruolo`
   ADD CONSTRAINT `ruolo_getsore` FOREIGN KEY (`ID_gestore`) REFERENCES `gestore` (`ID`);
+
+--
+-- Limiti per la tabella `sensore`
+--
+ALTER TABLE `sensore`
+  ADD CONSTRAINT `sensore_luogoaperto` FOREIGN KEY (`ID_luogoaperto`) REFERENCES `luogoaperto` (`ID`),
+  ADD CONSTRAINT `sensore_stanza` FOREIGN KEY (`ID_stanza`) REFERENCES `stanza` (`ID`);
 
 --
 -- Limiti per la tabella `stanza`
